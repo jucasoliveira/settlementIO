f323a1a857da96e97d65dc0bd0ceb3eb47855076
Lucas Oliveira <loliveira@cartrawler.com>
2020-07-01 18:02:09 +0100

    add rust poc

---
+++ poc_rust/target/debug/deps/poc_rust.d
@@ -0,0 +1,5 @@
+/Users/loliveira/Documents/settlementIO/poc_rust/target/debug/deps/poc_rust: src/main.rs
+
+/Users/loliveira/Documents/settlementIO/poc_rust/target/debug/deps/poc_rust.d: src/main.rs
+
+src/main.rs:
---
+++ poc_rust/target/debug/poc_rust.d
@@ -0,0 +1,1 @@
+/Users/loliveira/Documents/settlementIO/poc_rust/target/debug/poc_rust: /Users/loliveira/Documents/settlementIO/poc_rust/src/main.rs
